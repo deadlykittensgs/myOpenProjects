04dc4e20de67089728c12a4b51aaf16e36c40ee0
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-05-19 14:23:15 -0500

    react tailwind started

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Step from "./Step"
+import React from "react"
+
+export default function App() {
+
+
+  return(
+    <>
+    <Step/>
+    <Step/>
+    <Step/>
+    <Step/>
+    </>
+  )
+
+
+}
